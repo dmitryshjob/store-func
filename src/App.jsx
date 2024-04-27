835fc467d3a4c624b9e5343b576b562ff2b4cb61
Dmitry <dmitryshjod@yandex.ru>
2024-04-27 15:14:16 +0300

    store-func

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './App.css'
 
 
@@ -32,29 +31,6 @@ function ShopItemFunc() {
   );
 
 }
-
-// class ShopItemClass extends React.Component {
-//   render() {
-//     return (
-//       <div className='main-content'>
-//       <h2>{item.brand}</h2>
-//       <h1>{item.title}</h1>
-//       <h3>{item.description}</h3>
-//       <div className='description'>{item.descriptionFull}</div>
-//       <div className='highlight-window mobile'>
-//         <div className='highlight-overlay'></div>
-//       </div>
-//       <div className='divider'></div>
-//       <div className='purchase-info'>
-//         <div className='price'>{item.currency}{item.price}.00</div>
-//         <button>Добавить в корзину</button>
-//       </div>
-//     </div>
-//     )
-//   }
-// }
-
-
 
 function App() {
 
