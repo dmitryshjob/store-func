d2beb58321cb1482376d3f76ca4e1fb11d1d1481
Dmitry <dmitryshjod@yandex.ru>
2024-05-07 16:49:31 +0300

    store-func

--- src/App.jsx
+++ src/App.jsx
@@ -11,24 +11,25 @@ const item = {
   currency: '£'
 }
 
-function ShopItemFunc() {
+function ShopItemFunc( { item } ) {
+ 
   return (
-    <div className='main-content'>
+    <div className='main-content'> 
       <h2>{item.brand}</h2>
-      <h1>{item.title}</h1>
-      <h3>{item.description}</h3>
-      <div className='description'>{item.descriptionFull}</div>
+      <h1>{ item.title}</h1>
+      <h3>{ item.description}</h3>
+      <div className='description'>{ item.descriptionFull}</div>
       <div className='highlight-window mobile'>
         <div className='highlight-overlay'></div>
       </div>
       <div className='divider'></div>
       <div className='purchase-info'>
-        <div className='price'>{item.currency}{item.price}.00</div>
+        <div className='price' >{`${ item.currency}`}{`${ item.price}`}.00</div>
         <button>Добавить в корзину</button>
       </div>
     </div>
 
-  );
+);
 
 }
 
@@ -42,7 +43,7 @@ function App() {
         <div className='highlight-overlay'></div>
       </div>
       <div className="window">
-        <ShopItemFunc />
+        <ShopItemFunc item={item} />
       </div>
     </div>
   )
